6dfb7a3c1c19d4c37b9ab29be0fd17d9773906fc
ForesterSTI <mamogobokabelo@gmail.com>
2024-05-23 17:46:38 +0200

    Minor change

--- Home_prac/Thurday 23/Html website/express.js
+++ Home_prac/Thurday 23/Html website/express.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs').promises;
 const host = "localhost";
-const path = require('path');
+//const path = require('path');
 const port = 8080;
 
 
@@ -9,17 +9,18 @@ const requestListner = function listen(req, res){
 let requestURL = req.url
 switch(requestURL){
     case"/":
-        fs.readFile(__dirname+"public/index.html").then((content)=>{
+    fs.readFile(__dirname+"/index.html").then((content)=>{
         res.setHeader('Content-Type', 'text/html');
         res.writeHead(200)
         res.end(content)
     }).catch((err)=>{
         res.writeHead(200)
         res.end(err)
+        return
     })
     break
     case"/contact":
-    fs.readFile(__dirname + "public/contact.html").then((content)=>{
+    fs.readFile(__dirname + "/contact.html").then((content)=>{
         res.setHeader('Content-Type', 'text/html');
         res.writeHead(200)
         res.end(content)
